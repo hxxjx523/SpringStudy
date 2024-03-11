8185b2ce7d7fe1c2d211633d99df0de4ecaf266b
hxxjx523 <hhj05230@gmail.com>
2024-03-11 14:10:54 +0900

    bean

--- src/main/java/com/example/di/DiApplication.java
+++ src/main/java/com/example/di/DiApplication.java
@@ -2,12 +2,22 @@ package com.example.di;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class DiApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DiApplication.class, args);
-	}
 
+		ApplicationContext context = SpringApplication.run(DiApplication.class, args);
+		// 모든 Bean들의 이름 가져와서
+		String[] beanNames = context.getBeanDefinitionNames();
+		// 확인해보기
+		for (String beanName : beanNames) {
+			// 단, 기본적으로 스프링에서 제공하는 Bean들은 가급적 필터링해서 보여주기 (전부 다 걸러지진 않음)
+			if(!beanName.startsWith("org.") && !beanName.startsWith("spring.")) {
+				System.out.println(beanName);
+			}
+		}
+	}
 }
